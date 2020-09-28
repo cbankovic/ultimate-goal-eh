9f69e4219edca56da47a5602573074645ac2dedb
bankovicc <2u&Lfh2xR*-kcfZJzZY8>
2020-09-28 16:53:26 -0500

    initial commit

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/christian/ShooterTesting001.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.christian;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Concept: Shooter Testing 001", group = "Concept")
+class ShooterTesting001 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+
+    }
+}
