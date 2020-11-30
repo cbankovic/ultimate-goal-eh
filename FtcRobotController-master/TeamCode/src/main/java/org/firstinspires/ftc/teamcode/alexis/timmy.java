aa722950a1637e07d4a28c4ec93d77daab4a3d23
bankovicc <2u&Lfh2xR*-kcfZJzZY8>
2020-11-30 17:56:56 -0600

    Edited Alexis code

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alexis/timmy.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alexis/timmy.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.alexis;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp(name = "Chickeb Nugget", group = "Testing")
 public class timmy extends OpMode {
 
     /** Declare Hardware **/
@@ -16,28 +18,28 @@ public class timmy extends OpMode {
     @Override
     public void init() {
 
-        motorFrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        motorFrontLeft = hardwareMap.dcMotor.get("Front Left");
         motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorFrontLeft.setPower(0);
         motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        motorFrontRight = hardwareMap.dcMotor.get("FrontRight");
+        motorFrontRight = hardwareMap.dcMotor.get("Front Right");
         motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorFrontRight.setPower(0);
         motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        motorBackLeft = hardwareMap.dcMotor.get("BackLeft");
+        motorBackLeft = hardwareMap.dcMotor.get("Back Left");
         motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackLeft.setPower(0);
         motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        motorBackRight = hardwareMap.dcMotor.get("BackRight");
+        motorBackRight = hardwareMap.dcMotor.get("Back Right");
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setPower(0);
         motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -52,6 +54,8 @@ public class timmy extends OpMode {
         try {
             ProMotorControl(gamepad1.right_stick_y, -gamepad1.right_stick_x, -gamepad1.left_stick_x);
             telemetry.addData("Left_x", gamepad1.left_stick_x);
+            telemetry.addData("Right_x", gamepad1.right_stick_x);
+            telemetry.addData("Right_y", -gamepad1.right_stick_y);
 
         } catch (Exception ex){
             // Catching the exception
